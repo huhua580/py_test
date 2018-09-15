b8fbfc2a93b897ff861c27d80cb8e58f284bf030
huhua580 <516519323@qq.com>
2018-09-15 12:50:33 +0800

    09151250

---
+++ class/hellos.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+class Hello(object):
+    """docstring for Hello."""
+    def hello(self, name='world'):
+        print('Hello, %s' %name)
---
+++ class/orm.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#coding:utf-8
+from hellos import Hello
+h = Hello()
+h.hello()
+print(type(h))
